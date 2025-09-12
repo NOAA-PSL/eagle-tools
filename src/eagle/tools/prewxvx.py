fa2c89655472e0674bcb4e47579b032c02317ee2
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-09-12 11:10:11 -0700

    other sources for prewxvx

--- src/eagle/tools/prewxvx.py
+++ src/eagle/tools/prewxvx.py
@@ -12,13 +12,20 @@ def main(config):
     setup_simple_log()
 
     forecast_path = config["forecast_path"]
+    output_path = config["output_path"]
     model_type = config["model_type"]
     from_anemoi = config.get("from_anemoi", True)
     try:
-        assert model_type == "nested-lam"
-        assert from_anemoi
+        assert "lam" in model_type
     except:
         raise NotImplementedError
+
+    open_kwargs = {
+        "load": True,
+        "reshape_cell_to_2d": True,
+        "levels": config.get("levels", None),
+        "vars_of_interest": config.get("vars_of_interest", None),
+    }
 
     dates = pd.date_range(config["start_date"], config["end_date"], freq=config["freq"])
     for t0 in dates:
@@ -26,19 +33,24 @@ def main(config):
         logger.info(f"Processing {st0}")
 
         path_in = f"{forecast_path}/{st0}.240h.nc"
-        path_out= f"{forecast_path}/lam.{st0}.240h.nc"
+        path_out= f"{output_path}/lam.{st0}.240h.nc"
 
         logger.info(f"Opening {path_in}")
-        lds = open_anemoi_inference_dataset(
-            path=path_in,
-            model_type="nested-lam",
-            lam_index=64220,
-            levels=config.get("levels", None),
-            vars_of_interest=config.get("vars_of_interest", None),
-            load=True,
-            reshape_cell_to_2d=True,
-            lcc_info=config.get("lcc_info", None),
-        )
+        if from_anemoi:
+            lds = open_anemoi_inference_dataset(
+                path=path_in,
+                model_type=model_type,
+                lam_index=64220,
+                lcc_info=config.get("lcc_info", None),
+                **open_kwargs,
+            )
+        else:
+            lds = open_forecast_zarr_dataset(
+                config["forecast_path"],
+                t0=t0,
+                trim_edge=config.get("trim_forecast_edge", None),
+                **open_kwargs,
+            )
         for key in ["x", "y"]:
             if key in lds.coords:
                 lds = lds.drop_vars(key)

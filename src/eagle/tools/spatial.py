a16230fe4e9d0105cca14618fe589f748b967b53
timothyas <smith.timothy.andrew@gmail.com>
2025-09-29 16:11:00 -0600

    remove these docstrings to have one spot for them

--- src/eagle/tools/spatial.py
+++ src/eagle/tools/spatial.py
@@ -60,67 +60,7 @@ def mae(target, prediction, weights=1., keep_t0=False):
 def main(config):
     """Compute spatial maps of RMSE and MAE
 
-    \b
-    Note:
-        The arguments documented here are passed via a config dictionary.
-
-    \b
-    Config Args:
-        keep_t0 (bool, optional): If True, keeps the initial condition time (t0)
-            as a separate dimension in the output file. This can produce very large results
-            and requires a lot of memory. If False, the metrics
-            are averaged over all initial conditions. Defaults to False.
-
-    \b
-    Config Args common to metrics.py:
-        model_type (str): The type of model grid, one of: "global", "lam",
-            "nested-lam", "nested-global".
-            This determines how grid cell area weights, edge trimming, and coordinates are handled.
-        \b
-        verification_dataset_path (str): The path to the anemoi dataset with target data
-            used for comparison.
-        \b
-        forecast_path (str): The directory path containing the forecast datasets.
-        \b
-        output_path (str): The directory where the output NetCDF files will be saved, as
-            f"{output_path}/spatial.rmse.{model_type}.nc" and
-            f"{output_path}/spatial.mae.{model_type}.nc"
-            or if keep_t0=True, then as
-            f"{output_path}/spatial.rmse.perIC.{model_type}.nc" and
-            f"{output_path}/spatial.mae.perIC.{model_type}.nc"
-        \b
-        start_date (str): The first initial condition date to process, in any format
-            interpretable by pandas.date_range.
-        \b
-        end_date (str): The last initial condition date to process, in any format
-            interpretable by pandas.date_range.
-        \b
-        freq (str): The frequency string for generating the date range between
-            start_date and end_date (e.g., "6h"), passed to pandas.date_range.
-        \b
-        lead_time (str): A string representing the forecast lead time (e.g., "240h")
-            used as part of the forecast input filename.
-        \b
-        from_anemoi (bool, optional): If True, opens forecast data using the
-            anemoi inference dataset format. Otherwise, assumes layout of dataset
-            created by ufs2arco using a base target layout. Defaults to True.
-        \b
-        lam_index (int, optional): For nested models (e.g., model_type="nested-lam"), this integer
-            specifies the number of grid points belonging to the LAM domain.
-            Defaults to None.
-        \b
-        levels (list, optional): A list of vertical levels to subset from the
-            datasets. If None, all levels are used. Defaults to None.
-        \b
-        vars_of_interest (list[str], optional): A list of variable names to
-            include in the analysis. If None, all variables are used. Defaults to None.
-        \b
-        trim_edge (int, optional): Specifies the number of grid points to trim
-            from the edges of the verification dataset. Only used for LAM or Nested configurations.
-            Defaults to None.
-        \b
-        trim_forecast_edge (int, optional): Specifies the number of grid points to
-            trim from the edges of the forecast dataset. Defaults to None.
+    See ``eagle-tools spatial --help`` or cli.py for help
     """
     setup_simple_log()
 
